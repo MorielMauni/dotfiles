f82fb5f1040d1bf6768c47c9295eb54cf10c28e2
Moriel Mauni <morielmauni@gmail.com>
2025-06-05 20:21:54 +0000

    nvim confs- start

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+vim.g.snacks_animate = false
+vim.g.lazyvim_check_order = false
+
+vim.opt.ignorecase = true
+
+-- vim.opt.number = false -- line numbers
+vim.opt.relativenumber = false
+vim.opt.scrolloff = 8
---
+++ dot_config/nvim/lua/plugins/disable.lua
@@ -0,0 +1,5 @@
+return {
+	-- { "echasnovski/mini.pairs", enabled = false }, -- gives double "" \ '' \ () etc
+	{ "folke/noice.nvim", enabled = false },
+	{ "rcarriga/nvim-notify", enabled = false },
+}
