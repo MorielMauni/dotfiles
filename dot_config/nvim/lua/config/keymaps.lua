f8095cec20aadb42a6f1f7f9d109c3c7c56d7305
Moriel Mauni <morielmauni@gmail.com>
2025-06-11 19:35:46 +0000

    adding init.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Set leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
 
 -- For conciseness
 local opts = { noremap = true, silent = true }
